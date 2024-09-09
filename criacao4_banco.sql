f5360f9bbe1409d61b8d6d42bd8b588ebf0549ba
Vinicius Oliveira <oliveiravinicuis598@gmail.com>
2024-09-09 15:13:47 -0300

    Create criacao4_banco.sql

---
+++ criacao4_banco.sql
@@ -0,0 +1,27 @@
+/*
+SENAC DF - CEP Jessé Freire
+2024 . 07 . 276  - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares
+Profe Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com Mysol projeto - Clínica Odontológica OdontoVida */
+INSERT INTO Paciente (nome, data_nascimento, telefone, endereco, email) VALORES
+( ' Ana Silva ' , ' 1985-84-15 ' , ' 11987654321 ' , ' Rua das Flores, 123, são Paulo ' , ' ana.silva@email.com ' ),
+( ' Carlos Oliveira ' , ' 1990-86-30 ' , ' 21987654321 ' , ' Avenida Paulista, €56, São Paulo ' , ' carlos.oliveira@email.com ' ),
+( ' Fernanda Costa ' , ' 1982-12-12 ' , ' 31987654321 ' , ' Rua das Acácias, 789, Belo Horizonte ' , ' fernanda.costa@email.com ' ),
+( ' João Santos ' , ' 1978-83-21 ' , ' 41987654321 ' , ' Praça da Liberdade, 101, Belo Horizonte ' , ' joao.santos@email.com ' ),
+( ' Maria Souza ' , ' 1995-89-10 ' , ' 51987654321 ' , ' Rua do Mercado, 282, Rio de Janeiro ' , ' maria.souza@email.com ' ),
+( ' Pedro Lima ' , '25 -07-1988 ' , ' 61987654321 ' , ' Rua das Palmeiras, 303, Rio de Janeiro ' , ' pedro.lima@email.com ' ),
+( ' Paula Ferreira ' , ' 1992-11-11 ' , ' 71987654321 ' , ' Rua dos Pinheiros, 404, Porto Alegre ' , ' paula.ferreira@email.com ' ),
+( ' Roberto Almeida ' , ' 05-05-1986 ' , ' 81987654321 ' , ' Avenida dos Anjos, 505, Porto Alegre ' , ' roberto.almeida@email.com ' ),
+( ' Sofia Martins ' , '20 /01/1984 ' , ' 91987654321 ' , ' Rua das Orquídeas, 686, Curitiba ' , ' sofia.martins@email.com ' ),
+( ' Tiago Pereira ' , ' 1991-83-15 ' , ' 11976543210 ' , ' Rua dos Lírios, 707, Curitiba ' , ' tiago.pereira@email.com ' );
+INSERIR Dentista (nome, especialidade, telefone, email) VALORES
+( ' Dr: João Mendes ' , ' Ortodontia ' , ' 11912345678 ' , ' joao.mendes@email.com ' ),
+( ' Dra, Maria Oliveira ' , ' Endodontia ' , ' 21912345678 ' , ' maria.oliveira@email.com ' ,
+( ' Dr. Pedro siiva ' , ' Periodontia ' , ' 31912345678 ' , ' pedro.silva@email.com ' ),
+( ' Dra. Fernanda Santos ' , ' Odontopediatria ' , ' 41912345678 ' , ' fernanda.santos@email.com ' ),
+( ' ou, Paulo Lima ' , ' Implantes ' , ' 51912345678 ' , ' paulo.1imademail.com ' ),
+( ' Dra. Juliana Costa ' , ' Estética ' , ' 61912345678 ' , ' juliana.costa@email.com ' ),
+( ' Dr. Ricardo Almeida ' , ' Cirurgia Oral ' , ' 71912345678 ' , ' ricardo.almeida@email.com ' ),
+( ' Dra, Luana Martins ' , ' Clínica Geral ' , ' 81912345678 ' , ' luana.martins@email.com ' ,
+( ' Dr, Gabriel Pereira ' , ' Prótese Dentária ' , ' 91912345678 ' , ' gabriel.pereira@email.com ' ),
+( ' Dra. Laura Ferreira ' , ' Radiologia ' , ' 11923456789 ' , ' laura.ferreira@email.com ' );
