7111da1f6cc3933948c27b607422e4ef094c70ee
Vinicius Oliveira <oliveiravinicuis598@gmail.com>
2024-09-09 15:02:29 -0300

    Create criando_banco.sql

---
+++ criando_banco.sql
@@ -0,0 +1,12 @@
+/*
+SENAC DF - CEP Jessé Freire
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares
+Profe Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com MySQl
+Projeto - Clinica Odontológica OdontoVida */
+
+create database Odontovida;
+
+-- 1. Criação do Arquivo/Banco de Dados create database OdontoVida;
+-- 2. Acessando Arquivo/Banco de Dados 
+use OdontoVida;
